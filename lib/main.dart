3dc17e836f79e48dd07ae19ccf78f4474c70b3b7
breno <breno@DESKTOP-U63HCPD>
2019-06-20 11:53:02 -0300

    Feito o layout e os tratamentos de click.

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,10 @@
 import 'package:flutter/material.dart';
 
+import 'package:buscador_gifs/ui/home_page.dart';
+
 void main() {
-  runApp(MaterialApp(home: Home(),))
-}
-
-class Home extends StatefulWidget {
-  @override
-  _HomeState createState() => _HomeState();
-}
-
-class _HomeState extends State<Home> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Buscador de GIFs'),
-      ),
-    );
-  }
+  runApp(MaterialApp(
+    home: HomePage(),
+    theme: ThemeData(hintColor: Colors.white),
+  ));
 }
